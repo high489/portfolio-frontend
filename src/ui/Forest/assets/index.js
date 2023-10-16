d4cb854d49f87cb47bd437837690cf73cf722cb3
high489 <vitalii.matskaniuk@gmail.com>
2023-10-16 17:25:50 -0300

    Sidebar and Home styles updated
    Earth, navlinks and social links animation styles updated for coarse pointer (mobile devices).
    Sun animation added.
    House svg image added to Forest component.

--- src/ui/Forest/assets/index.js
+++ src/ui/Forest/assets/index.js
@@ -1,3 +1,4 @@
 export { ReactComponent as Sun } from './sun.svg';
 export { ReactComponent as Tree1 } from './tree1.svg';
 export { ReactComponent as Tree2 } from './tree2.svg';
+export { ReactComponent as House } from './house.svg';
--- src/ui/Forest/index.jsx
+++ src/ui/Forest/index.jsx
@@ -1,11 +1,12 @@
 import styles from './forest.module.scss';
 
-import { Sun, Tree1, Tree2 } from './assets';
+import { Sun, Tree1, Tree2, House } from './assets';
 
 const Forest = () => {
   return (
     <div className={styles.forest}>
       <Sun className={`${styles['forest-element']} ${styles.sun}`} />
+      <House className={`${styles['forest-element']} ${styles.house}`} />
       <Tree1 className={`${styles['forest-element']} ${styles.t1}`} />
       <Tree1 className={`${styles['forest-element']} ${styles.t2}`} />
       <Tree2 className={`${styles['forest-element']} ${styles.t3}`} />
