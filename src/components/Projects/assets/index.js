932ebf1a467c455e5828dc95531fcb4b52fd1db6
high489 <vitalii.matskaniuk@gmail.com>
2023-11-09 02:14:50 -0300

    Extra icons in projects assets deleted

--- src/components/Projects/assets/index.js
+++ src/components/Projects/assets/index.js
@@ -1,5 +1,1 @@
-export { ReactComponent as PetCoutriesImage } from './pet-countries.svg';
-export { ReactComponent as FuturePetsImage } from './future-pets.svg';
-export { ReactComponent as FuturePet1Image } from './future-pet1.svg';
-export { ReactComponent as FuturePet2Image } from './future-pet2.svg';
 export { ReactComponent as LinkIcon } from './link-icon.svg';
